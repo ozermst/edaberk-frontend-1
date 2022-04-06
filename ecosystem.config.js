fa3849c0377d2cfb849bc7cb1cfa7b6766c467e7
Mustafa Ozer <mustafa.ozer@cento.com.tr>
2022-04-06 19:00:53 +0300

    pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,15 +6,12 @@ module.exports = {
       args: "start",
       env_development: {
         NODE_ENV: "development",
-        ENV_PATH: ".env.development",
       },
       env_test: {
         NODE_ENV: "test",
-        ENV_PATH: ".env.test",
       },
       env_production: {
         NODE_ENV: "production",
-        ENV_PATH: ".env.production",
       },
     },
   ],
